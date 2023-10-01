c9b50928ef1689d115442ac8f0986150e340c43f
JekabsVanags <jecobv@gmail.com>
2023-10-01 13:18:32 +0300

    User model and DB

---
+++ app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ApplicationRecord
+  has_secure_password
+
+  enum rank: ["MZSK/GNT","SK/G", "DZSK/DZG", "ROV/LG", "VAD", "VIEDSK/VIEDG", "CITS"]
+  enum activity_statuss: ["aktīvs", "interesents", "vecbiedrs"]
+  enum sex: ["M", "F", "O"]
+  enum permission_level: ["BDR", "VAD", "VLD"]
+
+end
---
+++ db/migrate/20231001095733_create_users.rb
@@ -0,0 +1,22 @@
+class CreateUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :users do |t|
+      t.string :name, null: false
+      t.string :surname, null: false
+      t.integer :rank, null: false
+      t.boolean :promise, null: false
+      t.string :phone
+      t.string :email
+      t.integer :activity_statuss, null: false
+      t.decimal :membership_fee_bilance, null: false, default: 0
+      t.date :joined_date, null: false
+      t.date :birth_date
+      t.integer :sex
+      t.string :password_digest
+      t.integer :permission_level, null: false, default: 0
+      t.boolean :agreed_to_data_collection
+
+      t.timestamps
+    end
+  end
+end
