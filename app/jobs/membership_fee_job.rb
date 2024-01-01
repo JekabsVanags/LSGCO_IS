811490a2cf6d038a1f5ad59176b188f604a021e4
JekabsVanags <jecobv@gmail.com>
2024-01-01 19:06:46 +0200

    Mailer fix

--- app/jobs/membership_fee_job.rb
+++ app/jobs/membership_fee_job.rb
@@ -9,7 +9,7 @@ class MembershipFeeJob < ApplicationJob
       user.recalculate_bilance #Pārrēķinam lietotāja bilanci.
 
       if user.membership_fee_bilance < -30 #Ja cilvēks ir vairāk kā 30 eiro parādā, nosūta epastu. Reģistrē darbu cron_log.
-        UserMailer.membership_fee_late_email(user).deliver_later
+        UserMailer.membership_fee_late_email(user).deliver_now
 
         CronLog.create({ description: "MembershipFeeJob; User: #{user.id}; Bilance_before: #{bilance_before}; Bilance_after: #{user.membership_fee_bilance}; Notification_email_sent: true" })
       else
