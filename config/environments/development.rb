a9957ad5268c75f19246ae7564cc2634a311c34d
JekabsVanags <jecobv@gmail.com>
2024-03-13 09:59:02 +0200

    Email setup prod

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,21 +41,23 @@ Rails.application.configure do
   # Don't care if the mailer can't send.
   config.action_mailer.perform_deliveries = true
 
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
 
   config.action_mailer.perform_caching = false
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    address:         'smtp.gmail.com',
-    port:            587,
-    domain:          'gmail.com',
-    user_name:       'jecobv',
-    password:        'vzre kday oqdc apul',
-    authentication:  'plain',
-    enable_starttls: true,
-    open_timeout:    5,
-    read_timeout:    5 }
+  config.action_mailer.delivery_method = :letter_opener
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   address:         'smtp.gmail.com',
+  #   port:            587,
+  #   domain:          'skautiungaidas.lv',
+  #   user_name:       'jekabs.vanags@skautiungaidas.lv',
+  #   password:        'secret',
+  #   authentication:  'plain',
+  #   enable_starttls: true,
+  #   open_timeout:    5,
+  #   read_timeout:    5 }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
