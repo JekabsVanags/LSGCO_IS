3c6a412f73135afbe567392adca103be8bd13d6a
JekabsVanags <jecobv@gmail.com>
2024-06-13 17:31:37 +0300

    Added new fields to forms

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,5 @@
 class Event < ApplicationRecord
-  validates :name, :date_from, :date_to, :event_type, :registration_till, :volunteer_scope, :publishable, presence: true
+  validates :name, :date_from, :date_to, :event_type, :registration_till, :volunteer_scope, presence: true
 
   enum event_type: ["Nometne", "Pārgājiens", "Darba grupa", "Labais darbs", "Cits"]
   enum volunteer_scope: ["Vienība", "Ielūgtās vienības", "Organizācija"]
