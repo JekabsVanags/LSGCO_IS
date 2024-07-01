c633c0ec26f391a36fd0c0fc94c1adaad0604dca
JekabsVanags <jecobv@gmail.com>
2024-07-01 12:55:05 +0300

    AlpineJS added

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,7 @@
 import "@hotwired/turbo-rails"
 import "controllers"
+import "alpine-turbo-drive-adapter"
+import Alpine from 'alpinejs'
+
+window.Alpine = Alpine
+Alpine.start()
--- app/javascript/views/user_edit.js
+++
@@ -1,9 +0,0 @@
-function togglePasswordEddit() {
-  $('#password_reset_form').toggle(500);
-  $('#data_reset_form').toggle(500);
-  
-}
-
-$(document).ready(function() {
-  $('.form_toggle').click(() => togglePasswordEddit())
-})
